68899ff6de6b3402542f5515846e7b1473a71175
Shiro Sakai <shrsakai@gmail.com>
2014-11-25 19:14:19 +0900

    fix graph

--- plot_windows_perf.py
+++ plot_windows_perf.py
@@ -11,27 +11,67 @@ Python(全部入り)
 出力
  distフォルダ以下に png ファイルを出力
 """
+
 import sys
 import pandas as pd
-import numpy as np
 import re
 import matplotlib.pyplot as plt
+import os
 from matplotlib import font_manager, dates
 from matplotlib.ticker import ScalarFormatter
 
+from optparse import OptionParser
+
+usage = "usage: %prog [options] keyword"
+parser = OptionParser(usage)
+
+parser.add_option("-s", "--start",
+                  action="store",
+                  dest="start",
+                  type="int",
+                  metavar="HHMM",
+                  default=0,
+                  help="start time by HHMM")
+parser.add_option("-e", "--end",
+                  action="store",
+                  dest="end",
+                  type="int",
+                  metavar="HHMM",
+                  default=2359,
+                  help="start time by HHMM")
+
+parser.add_option("-c", "--core",
+                  action="store",
+                  dest="core",
+                  type="int",
+                  metavar="N",
+                  default=1,
+                  help="Number of cpu core")
+
+parser.add_option("-m", "--minutelocator",
+                  action="store",
+                  dest="ml",
+                  type="int",
+                  metavar="N",
+                  default=10,
+                  help="Minute Locator")
+
+(options, args) = parser.parse_args()
 ########## パラメータ 開始 ##########
 # CPUコア数
-CPU_CORE_NUM = 4
+CPU_CORE_NUM = options.core
 # pngファイル出力ディレクトリ
 DEFAULT_OUTPUT_DIR = 'dist'
 # '%m/%d %H:%M' - 12/31 11:59
 # '%H:%M:%S' - 11:59:59
-X_AXIS_FORMATTER = dates.DateFormatter('%H:%M:%S')
+X_AXIS_FORMATTER = dates.DateFormatter('%b %d %H:%M:%S')
 # MinuteLocator(interval=10) - 10分おき
 # HourLocator() - 1時間おき
 # DayLocator() - 1日おき
-X_AXIS_LOCATOR = dates.MinuteLocator(interval=10)
+X_AXIS_LOCATOR = dates.MinuteLocator(interval=options.ml)
 ########## パラメータ 終了 ##########
+
+
 
 def is_byte_column(column):
     if (re.search(r'.* Bytes.*', column) and re.search(r'.*% .*', column) == None ):
@@ -44,10 +84,36 @@ def is_byte_column(column):
         return False
 
 
-for file in sys.argv[1:]:
-    df = pd.read_csv(file, index_col=0, low_memory=False, na_filter=False)
+start_hour = options.start / 100
+start_min = options.start % 100
+end_hour = options.end / 100
+end_min = options.end % 100
+
+
+def time_filter(date):
+    if date.hour < start_hour:
+        return False
+    elif date.hour > end_hour:
+        return False
+    elif date.hour == start_hour:
+        if date.minute < start_min:
+            return False
+    elif date.hour == end_hour:
+        if date.minute > end_min:
+            return False
+    return True
+
+
+for file in args:
+    df = pd.read_csv(file, index_col=0, low_memory=False, na_filter=True, verbose=True)
     df.index = df.index.to_datetime()
+    selection = [d for d in df.index
+                 if time_filter(d)]
+    df = df.loc[selection]
+    #print(df)
+    count = 0
     for column in df.columns:
+        df[column] = df[column].convert_objects(convert_numeric=True)
         _fig = plt.figure(figsize=(12, 4))
         try:
             # init) start
@@ -82,7 +148,7 @@ for file in sys.argv[1:]:
                               df[column].tolist(),
                               alpha=0.75,
                               label=_tmp_label)
-            _fig.subplots_adjust(top=0.9, bottom=0.15, right=0.90, left=0.10)
+            _fig.subplots_adjust(top=0.9, bottom=0.25, right=0.98, left=0.1)
             _ax1.grid(True)
             _ax1.yaxis.grid(True, linestyle='-', which='major', color='grey')
             _ax1.yaxis.grid(True, linestyle='-', which='minor', color='lightgrey')
@@ -107,20 +173,30 @@ for file in sys.argv[1:]:
 
             # create graph) set legend
             plt.figlegend((_line), (('min: ' + str(_min) + ", avg: " + str(_mean) + ", max: " + str(_max) ), ),
-                          'lower center', prop=_prop, ncol=3)
+                          'lower center',
+                          #loc=(0.1, 0.70),
+                          prop=_prop,
+                          ncol=3)
 
-            print "\t".join([column, _title, str(_ylabel), str(_min), str(_max), str(df[column].dtype)])
-
+            # print "\t".join([column, _title, str(_ylabel), str(_min), str(_max), str(df[column].dtype)])
             # create graph) save png
+            date_str = df.index[0].strftime("%Y%m%d")
+
+            if not os.path.exists(DEFAULT_OUTPUT_DIR + '/' + os.path.basename(file)):
+                os.makedirs(DEFAULT_OUTPUT_DIR + '/' + os.path.basename(file))
             _column_str = str(column).replace('\\\\', '').replace('\\', '_')
-            plt.savefig(DEFAULT_OUTPUT_DIR + '/' + "".join(
+            time_str = '%s_%04d_%04d-' % (date_str, options.start, options.end)
+
+            filename = DEFAULT_OUTPUT_DIR + '/' + os.path.basename(file) + '/' + time_str + "".join(
                 [c for c in _column_str
                  if c.isalpha() or c.isdigit() or c == '_' or c == ' ' or c == '(' or c == ')']).rstrip()
-            )
+            plt.savefig(filename)
             _fig.clear()
             plt.close()
+            count += 1
+            print "output:", str(column)
         except:
             # skip error
             print "skip:", str(column)
-            _fig.clear()
+            #raise
             plt.close()
